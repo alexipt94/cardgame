af10bfb3ce8cde01cee8ba4bd2fd30a2ff9b8949
Alexey Iptyshev <hoeati1@gmail.com>
2026-05-09 13:29:23 +0700

    chore: configure biome, husky, lint-staged

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,18 +1,14 @@
-
-
 function App() {
-
-
   return (
-      <section id="center">
-        <div>
-          <h1>Стартовая страница</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-      </section>
-  )
+    <section id="center">
+      <div>
+        <h1>Стартовая страница</h1>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+        </p>
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
