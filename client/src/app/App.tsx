2e82fceb03c59b6b9e0c0a56a34133d7bdfc61be
Alexey Iptyshev <hoeati1@gmail.com>
2026-05-10 21:50:41 +0700

    feat: add Card component with props and types

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,11 +1,26 @@
+import { Card } from '../components/Card';
+
 function App() {
   return (
     <section id="center">
       <div>
-        <h1>Стартовая страница</h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-        </p>
+        <Card name="Goblin" cost={3} descr="Weak goblin" hp={50} attack={15} effects={['Kucha']} />
+        <Card
+          name="Knight"
+          cost={6}
+          descr="Emperor knight"
+          hp={150}
+          attack={45}
+          effects={['Razgon']}
+        />
+        <Card
+          name="Dargon"
+          cost={9}
+          descr="Green Dragon"
+          hp={320}
+          attack={65}
+          effects={['Splash attack', 'Dragon Breath', 'Dragon Blood']}
+        />
       </div>
     </section>
   );
