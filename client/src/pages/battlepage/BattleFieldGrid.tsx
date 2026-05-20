5df5959ca703c5b921769a4b8836bc1e3621e188
Alexey <hoeati1@gmail.com>
2026-05-20 18:20:23 +0700

    feat: add battlefield grid and arena background

---
+++ client/src/pages/battlepage/BattleFieldGrid.tsx
@@ -0,0 +1,21 @@
+export type BattlefieldCell = {
+  id: string;
+  row: number;
+  col: number;
+};
+
+export function createBattlefieldCells(rows: number, cols = 12): BattlefieldCell[] {
+  const cells: BattlefieldCell[] = [];
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      cells.push({
+        id: `${row}-${col}`,
+        row,
+        col,
+      });
+    }
+  }
+
+  return cells;
+}
--- client/src/pages/battlepage/BattlePage.tsx
+++ client/src/pages/battlepage/BattlePage.tsx
@@ -1,6 +1,10 @@
+//import { useState } from 'react';
+import { createBattlefieldCells } from './BattleFieldGrid';
 import './BattlePage.css';
-
 export function BattlePage() {
+  // const [isDraggingCard, setIsDraggingCard] = useState(false);
+  const rows = 4;
+  const cells = createBattlefieldCells(rows);
   return (
     <div className="battle-page">
       <header className="battle-header battle-section">
@@ -9,7 +13,17 @@ export function BattlePage() {
         <div className="battle-header-enemy">Boss</div>
       </header>
 
-      <main className="battle-field battle-section">Battlefield</main>
+      <main className="battle-field battle-section">
+        <img className="battlefield-bg" src="/assets/background/grass_bf.png" alt="" />
+
+        <div className={`battlefield-grid `} style={{ gridTemplateRows: `repeat(${rows}, 1fr)` }}>
+          {cells.map((cell) => (
+            <div key={cell.id} className="battlefield-cell">
+              {cell.row}:{cell.col}
+            </div>
+          ))}
+        </div>
+      </main>
 
       <section className="battle-chat battle-section">Chat</section>
       <section className="battle-deck battle-section">Deck</section>
